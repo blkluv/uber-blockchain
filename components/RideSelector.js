1feb5aa9b5460d1929037e9f05a84a2121d948bc
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-09-22 11:55:54 +0100

    setup sanity client, pull car list from sanity, hook to fetch data

--- components/RideSelector.js
+++ components/RideSelector.js
@@ -1,7 +1,9 @@
+import { useEffect, useContext, useState } from 'react'
 import Image from 'next/image'
 
 // Logos & Images
 import ethLogo from '../assets/eth-logo.png'
+
 import uberX from '../assets/rides/uberX.png'
 import uberBlack from '../assets/rides/uberBlack.png'
 import uberBlackSuv from '../assets/rides/uberBlackSuv.png'
@@ -22,36 +24,26 @@ const style = {
     price: `mr-[-0.8rem]`,
 }
 
-const carList = [
-    {
-        service: 'UberX',
-        iconUrl: uberX,
-        priceMultiplier: 1,
-    },
-    {
-        service: 'UberBlack',
-        iconUrl: uberBlack,
-        priceMultiplier: 1.5,
-    },
-    {
-        service: 'UberBlackSuv',
-        iconUrl: uberBlackSuv,
-        priceMultiplier: 1.5,
-    },
-    {
-        service: 'UberSelect',
-        iconUrl: uberSelect,
-        priceMultiplier: 1.5,
-    },
-    {
-        service: 'UberXL',
-        iconUrl: uberXL,
-        priceMultiplier: 1.5,
-    },
-]
-
 const basePrice = 1542
+
 const RideSelector = () => {
+
+    const [carList, setCarList] = useState([])
+
+    useEffect(() => {
+        ; (async () => {
+            try {
+                //try get data
+                const response = await fetch('/api/db/getRideTypes')
+                // set data
+                const data = await response.json()
+                setCarList(data.data)
+            } catch (error) {
+                console.error(error)
+            }
+        })()
+    }, [])
+
     return (
         <div className={style.wrapper}>
             <div className={style.title}>Choose a ride, or swipe up for more</div>
@@ -79,9 +71,10 @@ const RideSelector = () => {
                             <Image src={ethLogo} height={25} width={40} />
                         </div>
                     </div>
-                ))}
+                ))}                
             </div>
-        </div>    )
-}
+        </div>
+    )
+ }
 
 export default RideSelector
