f6fa62aa8fc60c0c8cd3b6d259e80d60bee27aba
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-09-22 12:00:48 +0100

    add markers 📌

--- components/LocationSelector.js
+++ components/LocationSelector.js
@@ -1,5 +1,6 @@
 import { useState } from 'react'
 import { useContext } from 'react'
+import { UberContext } from '../context/uberContext'
 
 const style = {
     wrapper: `pt-2`,
@@ -14,61 +15,62 @@ const style = {
 
 const LocationSelector = () => {
     const [inFocus, setInFocus] = useState('from')
-    const [pickup, setPickup] = useState('')
-    const [dropoff, setDropoff] = useState('')
+    const { pickup, setPickup, dropoff, setDropoff } = useContext(UberContext)
+    const { pickup, setPickup, dropoff, setDropoff } = useContext(UberContext)
 
     return (
         <div className={style.wrapper}>
-            <div className={style.searchHeader}>
-                {inFocus === 'from' ? 'Where can we pick you up?' : 'Where to?'}
+          <div className={style.searchHeader}>
+            {inFocus === 'from' ? 'Where can we pick you up?' : 'Where to?'}
+          </div>
+          <div className={style.inputBoxes}>
+            <div
+              className={`${style.inputBox} ${
+                inFocus === 'from' && style.focusedInputBox
+              }`}
+            >
+              <div className={style.svgContainer}>
+                <svg viewBox='0 0 24 24' width='1em' height='1em'>
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M12 14a2 2 0 100-4 2 2 0 000 4zm5-2a5 5 0 11-10 0 5 5 0 0110 0z'
+                  />
+                </svg>
+              </div>
+              <input
+                className={style.input}
+                placeholder='Enter pickup location'
+                value={pickup}
+                onChange={e => setPickup(e.target.value)}
+                onFocus={() => setInFocus('from')}
+              />
             </div>
-            <div className={style.inputBoxes}>
-                <div
-                    className={`${style.inputBox} ${inFocus === 'from' && style.focusedInputBox
-                        }`}
-                >
-                    <div className={style.svgContainer}>
-                        <svg viewBox='0 0 24 24' width='1em' height='1em'>
-                            <path
-                                fillRule='evenodd'
-                                clipRule='evenodd'
-                                d='M12 14a2 2 0 100-4 2 2 0 000 4zm5-2a5 5 0 11-10 0 5 5 0 0110 0z'
-                            />
-                        </svg>
-                    </div>
-                    <input
-                        className={style.input}
-                        placeholder='Enter pickup location'
-                        value={pickup}
-                        onChange={e => setPickup(e.target.value)}
-                        onFocus={() => setInFocus('from')}
-                    />
-                </div>
-                <div className={style.verticalLine} />
-                <div
-                    className={`${style.inputBox} ${inFocus === 'to' && style.focusedInputBox
-                        }`}
-                >
-                    <div className={style.svgContainer}>
-                        <svg viewBox='0 0 24 24' width='1em' height='1em'>
-                            <path
-                                fillRule='evenodd'
-                                clipRule='evenodd'
-                                d='M14 10h-4v4h4v-4zM7 7v10h10V7H7z'
-                            />
-                        </svg>
-                    </div>
-                    <input
-                        className={style.input}
-                        placeholder='Where to?'
-                        value={dropoff}
-                        onChange={e => setDropoff(e.target.value)}
-                        onFocus={() => setInFocus('to')}
-                    />
-                </div>
+            <div className={style.verticalLine} />
+            <div
+              className={`${style.inputBox} ${
+                inFocus === 'to' && style.focusedInputBox
+              }`}
+            >
+              <div className={style.svgContainer}>
+                <svg viewBox='0 0 24 24' width='1em' height='1em'>
+                  <path
+                    fillRule='evenodd'
+                    clipRule='evenodd'
+                    d='M14 10h-4v4h4v-4zM7 7v10h10V7H7z'
+                  />
+                </svg>
+              </div>
+              <input
+                className={style.input}
+                placeholder='Where to?'
+                value={dropoff}
+                onChange={e => setDropoff(e.target.value)}
+                onFocus={() => setInFocus('to')}
+              />
             </div>
+          </div>
         </div>
-    )
+      )
 }
-
 export default LocationSelector
--- pages/api/map/getLocationCoordinates.js
+++ pages/api/map/getLocationCoordinates.js
@@ -5,6 +5,8 @@ const getLocationCoordinates = async (req, res) => {
         const response = await fetch(mapboxUrl)
         const data = await response.json()
 
+        console.log(data.features[0].center)
+
         res.status(200).send({ message: 'success', data: data.features[0].center })
     } catch (error) {
         res.status(500).send({ message: 'error', data: error.message })
