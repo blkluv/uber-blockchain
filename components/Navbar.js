5854ca04eacc1f2d0e6a5577f51230c82f368c87
gregoryann <32854050+gregoryann@users.noreply.github.com>
2022-09-22 12:04:34 +0100

    metamask authentiaction + save to sanity db

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,6 +2,10 @@ import Image from 'next/image'
 import avatar from '../assets/icons/avatar.png'
 import { BsPerson } from 'react-icons/bs'
 
+import { useContext } from 'react'
+import { UberContext } from '../context/uberContext'
+
+
 const style = {
     wrapper: `h-16 w-full bg-black text-white flex md:justify-around items-center px-60 fixed z-20`,
     leftMenu: `flex gap-3`,
@@ -14,9 +18,11 @@ const style = {
     loginText: `ml-2`,
 }
 
-const currentAccount = '0xe276941FBd5f936E677dB9B6eEE8212a3b268C5E'
 
 const Navbar = () => {
+
+    const { currentAccount, connectWallet, currentUser } = useContext(UberContext)
+
 
     return (
         <div className={style.wrapper}>
@@ -30,7 +36,7 @@ const Navbar = () => {
 
             <div className={style.rightMenu}>
                 <div className={style.menuItem}>Help</div>
-                <div className={style.menuItem}>G3P</div>
+                <div className={style.menuItem}>{"G3P"}</div>
                 <div className={style.userImageContainer}>
                     <Image
                         className={style.userImage}
@@ -39,19 +45,7 @@ const Navbar = () => {
                         height={40}
                     />
                 </div>
-                {currentAccount ? (
-                    <div>
-                        {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
-                    </div>
-                ) : (
-                    <div className={style.loginButton} onClick={() => connectWallet()}>
-                        <BsPerson />
-                        <span className={style.loginText}>Log in</span>
-                    </div>
-                )}
-            </div>
-        </div>
-    )
+             )
 }
 
 export default Navbar
